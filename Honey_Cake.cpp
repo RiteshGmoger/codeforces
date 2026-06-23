be8fff734280f83996605fcd658d80b99d8adf40
Ratan <ratanmoger99@gmail.com>
2026-06-23 13:05:05 +0530

    solved 1 cf problem

---
+++ Honey_Cake.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using i128  = __int128;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+
+#define all(x)   (x).begin(),(x).end()
+#define rall(x)  (x).rbegin(),(x).rend()
+#define pb       push_back
+#define eb       emplace_back
+#define ff       first
+#define ss       second
+
+
+void solve()
+{
+	ll w{},h{},d{},n{}; cin>>w>>h>>d>>n;
+
+	ll wp = gcd(n,w);
+	n /= wp;
+	ll hp = gcd(n,h);
+	n /= hp;
+	ll dp = gcd(n,d);
+	n /= dp;
+
+	if(n != 1) cout<<-1;
+	else cout<<wp-1<<' '<<hp-1<<' '<<dp-1<<'\n';
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t{1};
+	//cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
