bfd811bf455fc415abcb5b3735428650a004fa94
Ritesh G. Moger <ratanmoger99@gmail.com>
2026-08-02 23:01:07 +0530

    solved 1 1200 rated problem

---
+++ Basils_Garden.cpp
@@ -0,0 +1,43 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using i128  = __int128;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+
+#define all(x)   (x).begin(),(x).end()
+#define rall(x)  (x).rbegin(),(x).rend()
+#define pb       push_back
+#define eb       emplace_back
+#define ff       first
+#define ss       second
+
+
+void solve()
+{
+	int n{}; cin>>n;
+	vll h(n); for(auto& it : h) cin>>it;
+
+	long long ans = h[n-1];
+	for(int i{n-2};i>=0;--i) ans = max(ans+1,h[i]);
+
+	cout<<ans<<'\n';
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t{1};
+	cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
