109cbca40bed4ccf3f364faab9f3f58e473c4f69
Ritesh G. Moger <ratanmoger99@gmail.com>
2026-08-16 15:47:42 +0530

    solved 1 problem

---
+++ Binary_string_Battle.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using i128  = __int128;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+
+#define all(x)   (x).begin(),(x).end()
+#define rall(x)  (x).rbegin(),(x).rend()
+#define pb       push_back
+#define eb       emplace_back
+#define ff       first
+#define ss       second
+
+
+void solve()
+{
+	int n{},k{}; cin>>n>>k;
+	string s{}; cin>>s;
+
+	int one{};
+	for(int i{};i<n;++i)
+		if(s[i] == '1') one++;
+	
+	if(one <= k || n < 2*k)
+		cout<<"Alice\n";
+	else
+		cout<<"Bob\n";
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t{1};
+	cin >> t;
+	while(t--) solve();
+
+	return 0;
+}
